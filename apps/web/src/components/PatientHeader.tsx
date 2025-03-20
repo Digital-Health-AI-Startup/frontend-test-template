5c4a72488c4214437a54fe8a6654b7fff5eb5566
ivana-matijevic <ivanamatijevic@mac.mynetworksettings.com>
2025-03-20 11:01:48 -0400

    Adds base UI

---
+++ apps/web/src/components/PatientHeader.tsx
@@ -0,0 +1,32 @@
+import { Box, Stack, Typography } from "@mui/material";
+import { FallbackUserAvatar } from "./FallbackUserAvatar";
+
+export const PatientHeader = () => {
+	return (
+		<Box
+			sx={(theme) => ({
+				background: theme.palette.common.white,
+				p: 4,
+			})}
+		>
+			<Stack
+				sx={{
+					display: "flex",
+					flexDirection: "row",
+					alignItems: "center",
+					height: "75px",
+				}}
+			>
+				<FallbackUserAvatar />
+				<Box sx={{ ml: 1.5 }}>
+					<Typography variant="h6" fontWeight={700}>
+						Smith, Jane
+					</Typography>
+					<Typography variant="body2" color="textSecondary">
+						05/04/1995
+					</Typography>
+				</Box>
+			</Stack>
+		</Box>
+	);
+};
